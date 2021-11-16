256cdabb96f416ece5136ff21e458212ebffebc1
Micailde Junior <micaoliveira.tecnologo@gmail.com>
2021-11-16 20:07:43 -0300

    Add project files.

---
+++ Project/Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿namespace Project.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+
+
+
+    }
+}
